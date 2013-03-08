cde7d3453dc694d16b1570b58a1260a2289ce882
Emily Ann Gutierrez <emilygutierrez86@gmail.com>
2013-03-07 22:56:05 -0500

    Adding comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
   attr_accessible :description, :link
+
+  validates :description, presence: true
+  validates :link, presence: true,
+  				   format: /^http:\/\/.+\.com$/	
+
+  has_many :comments
 end
